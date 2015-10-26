9f3e632d56027bd88b25d9f0323711a465f0bc80
Bevis <bevis@binodata.com>
2015-10-27 02:01:02 +0800

    Code 整理，重新擷取方法。

--- PotterShopCar.Tests/BuyinyPotterBookTest.cs
+++ PotterShopCar.Tests/BuyinyPotterBookTest.cs
@@ -14,10 +14,7 @@ namespace PotterShopCar.Tests
             //Arrange
             int expect = 100;
             Customer customer = new Customer();
-            Book book = new Book();
-            book.price = 100;
-            book.Name = "Potter 1";
-            book.episode = 1;
+            Book book = GetSingleEpisode();
             customer.Buy(book);
 
             //Act
@@ -28,12 +25,35 @@ namespace PotterShopCar.Tests
             Assert.AreEqual(expect, actual);
         }
 
+        private static Book GetSingleEpisode()
+        {
+            Book book = new Book();
+            book.price = 100;
+            book.Name = "Potter 1";
+            book.episode = 1;
+            return book;
+        }
+
         [TestMethod]
         public void Test_Customer_Buy_First_and_Second_Episode_of_Potter_Price_Should_be_190()
         {
-              //Arrange
+            //Arrange
             int expect = 190;
             Customer customer = new Customer();
+            List<Book> books = GetTwoDifferentEpisode();
+
+            customer.Buy(books);
+
+            //Act
+            Employee employee = new Employee();
+            int actual = employee.GetPrice(customer.BuyingBook);
+
+            //Assert
+            Assert.AreEqual(expect, actual);
+        }
+
+        private static List<Book> GetTwoDifferentEpisode()
+        {
             Book book = new Book();
             book.price = 100;
             book.Name = "Potter 1";
@@ -45,15 +65,7 @@ namespace PotterShopCar.Tests
             List<Book> books = new List<Book>();
             books.Add(book);
             books.Add(book2);
-
-            customer.Buy(books);
-
-            //Act
-            Employee employee = new Employee();
-            int actual = employee.GetPrice(customer.BuyingBook);
-
-            //Assert
-            Assert.AreEqual(expect, actual);
+            return books;
         }
 
 
