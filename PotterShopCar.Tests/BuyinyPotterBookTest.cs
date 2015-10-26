915a4b599ccaab6d83f9fab4184649bd32557275
Bevis <bevis@binodata.com>
2015-10-27 01:38:20 +0800

    撰寫第一個情境的測試案例 依照測試案例建立相關會用到的物件與方法 目前測試錯誤

---
+++ PotterShopCar.Tests/BuyinyPotterBookTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using PotterShopCarLib;
+
+namespace PotterShopCar.Tests
+{
+    [TestClass]
+    public class BuyinyPotterBookTest
+    {
+        [TestMethod]
+        public void Test_Customer_Buy_First_Episode_of_Potter_Book()
+        {
+            //Arrange
+            int expect = 100;
+            Customer customer = new Customer();
+            Book book = new Book();
+            customer.Buy(book);
+
+            //Act
+            Employee employee = new Employee();
+            int actual = employee.GetPrice(customer.BuyingBook);
+
+            //Assert
+            Assert.AreEqual(expect, actual);
+        }
+
+
+
+
+    }
+
+       
+}
---
+++ PotterShopCarLib/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShopCarLib
+{
+    public class Employee
+    {
+        public int GetPrice(List<Book> list)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
